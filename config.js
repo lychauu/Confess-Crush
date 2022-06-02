6df0fc45dbbf9c37d50b8088ea9eb2d3f2e55b13
lychauu <106758696+lychauu@users.noreply.github.com>
2022-06-02 22:05:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    titleWeb: "chào nhee",
+    introTitle: 'hiiii',
+    introDesc: 
+         mưa rồi sao chưa đổ t đi =))
+    Lạc
+ơng`,
+    btnIntro: '^^hok',
+    title: '🥰',
+    desc: 'bạn yêu mình đúng khonggg',
+    btnYes: 'yêu lắm hâh,
+    btnNo: 'Không=)),
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'vì bạn cư tê💗',
+    mess: 'biết rồi kkk, không phải chối'
+    messDesc: 'thế có yêu k nói đi đừng sợ.',
+    btnAccept: 'có nhé <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
